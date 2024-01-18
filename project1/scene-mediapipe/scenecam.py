d264880e37aa9a23b79e43ad7ec8f92ed1cd84ba
Juno <jvmeifert@gmail.com>
2024-01-18 18:25:08 -0500

    label objects in scene

--- project1/scene-mediapipe/scenecam.py
+++ project1/scene-mediapipe/scenecam.py
@@ -23,12 +23,38 @@ def main():
 
 		# get detections
 		for scene_object in scene_objects.detections:
+
 			for category in scene_object.categories:
-				if category.score > 0.5:
-					print(category.category_name)
-					print("\n")
-
-
+				if category.score > 0.4:
+					box = scene_object.bounding_box
+					# bounding box
+					cv2.rectangle(
+						frame,
+						(box.origin_x, box.origin_y),
+						(box.origin_x + box.width, box.origin_y + box.height),
+						(0, 255, 0),
+						2
+					)
+					# text shadow
+					cv2.putText(
+						frame,
+						category.category_name,
+						(box.origin_x + 2, box.origin_y + 2),
+						0,
+						1.0,
+						(0,0,0),
+						2
+					)
+					# text
+					cv2.putText(
+						frame,
+						category.category_name,
+						(box.origin_x, box.origin_y),
+						0,
+						1.0,
+						(0,255,0),
+						2
+					)
 
 		# display frame
 		cv2.imshow("title", frame)
